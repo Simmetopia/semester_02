1f264040a9f30b5bee34861e271b95d9240d2785
Simmetopia <egeberg.simon@gmail.com>
2016-12-12 10:11:14 +0100

    Merge remote-tracking branch 'refs/remotes/origin/master' into SW-til-modtager
    # Conflicts:
    #	SMS/SMS.pro.user

---
+++ KodeTilHardware/Modtager/SWmodtager/SWmodtager.ino
@@ -0,0 +1,21 @@
+#include <StandardCplusplus.h>
+#include <system_configuration.h>
+#include <unwind-cxx.h>
+#include <utility.h>
+#include <vector>
+#define ZEROCROSS 19
+#define d_in 18
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
