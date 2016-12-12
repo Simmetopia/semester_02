ab3d4f5f53177afeaaa375bd21382410ac1bb497
Simmetopia <egeberg.simon@gmail.com>
2016-12-12 14:14:57 +0100

    hardware code

--- KodeTilHardware/Modtager/SWmodtager/SWmodtager.ino
+++ KodeTilHardware/Modtager/SWmodtager/SWmodtager.ino
@@ -3,19 +3,76 @@
 #include <unwind-cxx.h>
 #include <utility.h>
 #include <vector>
-#define ZEROCROSS 19
-#define d_in 18
+#define ZEROCROSS 2
+#define d_in 3
+#define redLed 32
+#define yellowLed 33
+#define greenLed 31
 
+std::vector<int> bufferVec;
 
-
-
+void dataIn();
+void writeToVector(unsigned char);
 
 void setup() {
-  // put your setup code here, to run once:
-
+pinMode(redLed, OUTPUT);
+pinMode(yellowLed, OUTPUT);
+pinMode(greenLed, OUTPUT);
+pinMode(ZEROCROSS, INPUT);
+pinMode(d_in, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+
+void loop() {
+  
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, HIGH);
+  
+  if(digitalRead(ZEROCROSS) == 1 && digitalRead(d_in) == 1)
+  {
+    dataIn();
+    digitalWrite(redLed, HIGH);
+  }
+}
+
+
+
+void writeToVector(unsigned char i) 
+{
+  if(i==0)
+  {
+    bufferVec.push_back(0);
+  }
+  else if(i==1)
+  {
+    bufferVec.push_back(1);
+  }
+  else
+  {
+    bufferVec.erase(bufferVec.begin(), bufferVec.end() );
+  }
+}
+
+void dataIn()
+{
+    digitalWrite(greenLed, LOW);
+    unsigned int count = 0;
+    while(count != 700)
+    {
+      digitalWrite(yellowLed, HIGH);
+      if(digitalRead(ZEROCROSS) == HIGH && digitalRead(d_in) == HIGH)
+      {
+        writeToVector(1);
+        count++;
+        delay(3);
+      }
+      if(digitalRead(ZEROCROSS) == HIGH && digitalRead(d_in) == LOW)
+      {
+        writeToVector(0);
+        count++;
+        delay(3);
+      }
+    } //end while
+    digitalWrite(yellowLed, LOW);
 }
