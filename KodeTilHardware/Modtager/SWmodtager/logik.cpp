e63f67458f480c872fcd3c603f58e6662eb21875
Simmetopia <egeberg.simon@gmail.com>
2016-12-13 19:48:51 +0100

    Modtager kode er nu complete

--- KodeTilHardware/Modtager/SWmodtager/logik.cpp
+++ KodeTilHardware/Modtager/SWmodtager/logik.cpp
@@ -1,7 +1,8 @@
 #include "logik.h"
 #include <vector>
 
-logik::logik(){ }
+logik::logik(){
+  }
 
 logik::~logik(){ }
 
@@ -9,6 +10,19 @@ logik::~logik(){ }
 void logik::readToVectorModtager(std::vector<int> a)
 {
 	tidsBin = a;
+}
+
+int logik::binaryConverter()
+{
+	int x = 1;
+	int tal = 0;
+	for (int i = tidsBin.size(); i > 0 ; --i)
+	{		
+		tal =+ tidsBin[i] * x;
+    x=x*2;
+	}
+
+	return tal-1;
 }
 
 
